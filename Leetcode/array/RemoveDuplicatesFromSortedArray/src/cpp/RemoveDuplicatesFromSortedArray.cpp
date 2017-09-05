8037ffd7c1d4af54293c8db1bbf60970dab95623
Richard-coder <zhi_zhuce@126.com>
2017-09-05 14:07:44 +0800

    文件目录设计

---
+++ Leetcode/array/RemoveDuplicatesFromSortedArray/src/cpp/RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+  public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        if (nums.empty())
+            return 0;
+
+        int index = 0;
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[index] != nums[i])
+                 nums[++index] = nums[i];
+                    
+        }
+         return index + 1;
+            
+    }
+};
